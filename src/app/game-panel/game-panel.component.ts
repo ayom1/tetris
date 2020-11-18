5ff324acb89a57770e0c6e059746a30695eb4e08
Ali Ahmed <aliahmed@Alis-MacBook-Pro.local>
2020-11-19 02:25:15 +0300

    movement commit

--- src/app/game-panel/game-panel.component.ts
+++ src/app/game-panel/game-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-game-panel',
@@ -16,14 +17,129 @@ export class GamePanelComponent implements OnInit {
       this.square[i]=[];
       for(let j=0;j<this.WIDTH;j++){
         this.square[i][j] = new Square(false);
-        this.square[i][j].backGround = 'red';
       }  
     }
     this.selectedShape = new Shape(1);
+    this.drawSquares();
+    // x--i,y--j 
+  }
+  onKey(event: any) { // without type info
+    console.log( (this.selectedShape.locationX+1)+'---'+(this.selectedShape.locationY-1));
+    console.log( (this.selectedShape.locationX+1)+'---'+this.selectedShape.locationY);
+    //move left
+    this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill=false;
+    this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill=false;
+    this.selectedShape.locationX--;
+    this.drawSquares();
+  }
+ drawSquares(){
+   //console.log(this.selectedShape.locationY+' ' +this.selectedShape.locationX);
+   if(this.selectedShape.type==1){
+      this.drawSquareShape();
+   }else if(this.selectedShape.type==2){
+    this.drawRightLShape();
+   }else{
+      
+   }
+  }
+  drawRightLShape() {
+
+    if(this.selectedShape.locationY==0){
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[2][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[2][1].fill;
+    }else if(this.selectedShape.locationY==1){
+      
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[2][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[2][1].fill;
+    }else if(this.selectedShape.locationY==2){
+
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[2][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[2][1].fill;
+    }else if(this.selectedShape.locationY==3){
+      this.square[0][this.selectedShape.locationX].fill = false;
+      this.square[0][this.selectedShape.locationX+1].fill = false;
+
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[2][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[2][1].fill;
+    }else if(this.selectedShape.locationY==4){
+      this.square[0][this.selectedShape.locationX].fill = false;
+      this.square[0][this.selectedShape.locationX+1].fill = false;
+      this.square[1][this.selectedShape.locationX].fill = false;
+      this.square[1][this.selectedShape.locationX+1].fill = false;
+
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[2][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[2][1].fill;
+    }else{
+      this.square[this.selectedShape.locationY-3][this.selectedShape.locationX].fill = false;
+      this.square[this.selectedShape.locationY-3][this.selectedShape.locationX+1].fill = false;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX].fill = false;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX+1].fill = false;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = false;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = false;
+      
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[2][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[2][1].fill;
+    }
+  }
+  drawSquareShape() {
+    console.log(this.selectedShape.locationY);
+    if(this.selectedShape.locationY==0){
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+    }else if(this.selectedShape.locationY==1){
+      
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+    }else if(this.selectedShape.locationY==2){
+      this.square[0][this.selectedShape.locationX].fill = false;
+      this.square[0][this.selectedShape.locationX+1].fill = false;
+      this.square[1][this.selectedShape.locationX].fill = false;
+      this.square[1][this.selectedShape.locationX+1].fill = false;
+
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+    }else{
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX].fill = false;
+      this.square[this.selectedShape.locationY-2][this.selectedShape.locationX+1].fill = false;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = false;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = false;
+      
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX].fill = this.selectedShape.squares[0][0].fill;
+      this.square[this.selectedShape.locationY-1][this.selectedShape.locationX+1].fill = this.selectedShape.squares[0][1].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX].fill = this.selectedShape.squares[1][0].fill;
+      this.square[this.selectedShape.locationY][this.selectedShape.locationX+1].fill = this.selectedShape.squares[1][1].fill;
+    }
   }
 
   ngOnInit(): void {
-      
+    interval(1000).subscribe(x => {
+      if(this.selectedShape.locationY<19){
+      this.selectedShape.locationY++;
+      this.drawSquares();
+    }
+  });
   }
 
 }
@@ -32,8 +148,10 @@ export class Shape{
   squares: Square[][];
   locationX:number;
   locationY:number;
+  type:number;
   constructor(private s:number){
-    if(s==1){
+    this.type=s;
+    if(s==1){ // square shape
       this.squares =  [];
       this.squares[0] = [];
       this.squares[0][0] = new Square(true);
@@ -41,6 +159,17 @@ export class Shape{
       this.squares[1] = [];
       this.squares[1][0] = new Square(true);
       this.squares[1][1] = new Square(true);
+    }else if(s==2){ // right l shape
+      this.squares =  [];
+      this.squares[0] = [];
+      this.squares[0][0] = new Square(false);
+      this.squares[0][1] = new Square(true);
+      this.squares[1] = [];
+      this.squares[1][0] = new Square(false);
+      this.squares[1][1] = new Square(true);
+      this.squares[2] = [];
+      this.squares[2][0] = new Square(true);
+      this.squares[2][1] = new Square(true);
     }else{
       this.squares =  [];
     }
